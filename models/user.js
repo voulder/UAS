56e7530204cd8a5368b1b7c01c302b7cb9a615b1
voulder <andy.putra98@gmail.com>
2022-12-03 18:20:35 +0700

    postproject fix

--- models/user.js
+++ models/user.js
@@ -1,25 +1,25 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema( {
-	
-	unique_id: Number,
-	email: String,
-	username: String,
-	password: {
-		type	: String,
-		required: true,
-		bcrypt	: true
-	}
-}),
-User = mongoose.model('User', userSchema);
+var userSchema = new Schema({
 
-projectSchema = new Schema( {
-	
-	unique_id: Number,
-	project: String,
-	description: String
-}),
-Project = mongoose.model('Project', projectSchema);
+    unique_id: Number,
+    email: String,
+    username: String,
+    password: {
+      type: String,
+      required: true,
+      bcrypt: true
+    }
+  }),
+  User = mongoose.model('User', userSchema);
+
+projectSchema = new Schema({
+
+    unique_id: Number,
+    project: String,
+    description: String
+  }),
+  Project = mongoose.model('Project', projectSchema);
 
 module.exports = User;
